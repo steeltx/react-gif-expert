62ffd2f8e162be227990d2215fc07e282dd173de
steeltx <realsteel.gnu@gmail.com>
2023-11-20 19:44:45 -0600

    Configuración inicial

---
+++ src/GitExpertApp.jsx
@@ -0,0 +1,5 @@
+export const GitExpertApp = () => {
+	return (
+		<h1>GitExpertApp</h1>
+	)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GitExpertApp } from './GitExpertApp';
+
+// agregar los estilos globales
+import './styles.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GitExpertApp />
   </React.StrictMode>,
 )
